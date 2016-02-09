ba069eca8f06aad98d07c7182f1f55f6d0ea290b
Cosmos Darkstar <cosmosdarkstarbrony@gmail.com>
2016-02-08 19:26:15 -0700

    156

---
+++ sr/skewll/ch14/sterter/Binery.java
@@ -0,0 +1,11 @@
+package skewll.ch14.sterter;
+
+public class Binery {
+
+	/*
+	 * Consider the following speedup of the insertion sort algorithm of Special
+	 * Topic 14.2. For each element, use the enhanced binary search algorithm
+	 * that yields the insertion position for missing elements. Does this
+	 * speedup have a significant impact on the efficiency of the algorithm?
+	 */
+}
