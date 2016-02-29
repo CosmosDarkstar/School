b8a3ee01c8214e236745bee0ce0f13b5f3e167f8
Cosmos <guitarnerd777@gmail.com>
2016-02-29 13:29:51 -0700

    df

---
+++ sr/skewll/queueszz/WorkOrder.java
@@ -0,0 +1,26 @@
+package skewll.queueszz;
+
+public class WorkOrder implements Comparable {
+	private int priority;
+	private String description;
+
+	public WorkOrder(int aPriority, String aDescription) {
+		priority = aPriority;
+		description = aDescription;
+	}
+
+	public String toString() {
+		return "priority=" + priority + ", description=" + description;
+	}
+
+	public int compareTo(Object otherObject) {
+		WorkOrder other = (WorkOrder) otherObject;
+		if (priority < other.priority) {
+			return -1;
+		} else if (priority > other.priority) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
