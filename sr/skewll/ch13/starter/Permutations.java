c9efbb1bcbe2be998cf9fe0580d01e7f4a602819
Cosmos <guitarnerd777@gmail.com>
2016-04-25 13:54:09 -0600

    amraaruarum

--- sr/skewll/ch13/starter/Permutations.java
+++ sr/skewll/ch13/starter/Permutations.java
@@ -1,15 +1,13 @@
 package skewll.ch13.starter;
 
-import java.util.ArrayList;
-
 /**
  * This program computes permutations of a string.
  */
 public class Permutations {
 	public static void main(String[] args) {
-		for (String s : permutations("eat")) {
-			System.out.println(s);
-		}
+		// for (String s : permutations("eat")) {
+		// System.out.println(s);
+		// }
 	}
 
 	/**
@@ -23,26 +21,21 @@ public class Permutations {
 		// ArrayList<String> result = new ArrayList<String>();
 		T largest = t[0];
 		// The empty string has a single permutation: itself
-		if (word.length() == 0) {
-			result.add(word);
-			return result;
-		} else {
-			// Loop through all character positions
-			for (int i = 0; i < word.length(); i++) {
-				// Form a shorter word by removing the ith character
-				String shorter = word.substring(0, i) + word.substring(i + 1);
-
-				// Generate all permutations of the simpler word
-				ArrayList<String> shorterPermutations = permutations(shorter);
-
-				// Add the removed character to the front of
-				// each permutation of the simpler word,
-				for (String s : shorterPermutations) {
-					result.add(word.charAt(i) + s);
-				}
-			}
-			// Return all permutations
-			return result;
-		}
+		/*
+		 * if (word.length() == 0) { result.add(word); return result; } else {
+		 * // Loop through all character positions for (int i = 0; i <
+		 * word.length(); i++) { // Form a shorter word by removing the ith
+		 * character String shorter = word.substring(0, i) + word.substring(i +
+		 * 1);
+		 * 
+		 * // Generate all permutations of the simpler word ArrayList<String>
+		 * shorterPermutations = permutations(shorter);
+		 * 
+		 * // Add the removed character to the front of // each permutation of
+		 * the simpler word, for (String s : shorterPermutations) {
+		 * result.add(word.charAt(i) + s); } } // Return all permutations return
+		 * result; }
+		 */
+		return largest;
 	}
 }
