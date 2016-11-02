22117ad9d90faa4c178c5d7f031a7cddc7454103
9507970 <9507970@646-0303-vpc011.graniteschools.org>
2016-11-02 13:27:18 -0600

    srn

---
+++ sr/skewll/ch07/Litht.java
@@ -0,0 +1,34 @@
+package skewll.ch07;
+
+import java.util.ArrayList;
+
+public class Litht {
+
+	public static void main(String[] args) {
+		// 35
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		primes.add(2);
+		primes.add(3);
+		primes.add(5);
+		primes.add(7);
+		primes.add(11);
+
+		// 36
+		for (int i = primes.size(); i >= 0; i++) {
+			System.out.println(primes.get(i - 1));
+		}
+
+		// 37. Ann, Cal
+
+		// 38. names is not actually initialized
+
+		// 39. names1: Emily,Bob,Cindy names2: Dave
+
+		// 40. Array
+
+		// 41. It will actually run? I'm not sure seeing as the book has
+		// directed me to files that are not pertinent to the question.
+
+	}
+
+}
